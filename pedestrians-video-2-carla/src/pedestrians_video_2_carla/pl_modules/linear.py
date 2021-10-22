72ddbec1cc363a1e1c570e7c9f369d219319615b
Maciej Wielgosz <wielgosz@agh.edu.pl>
2021-10-22 18:28:14 +0200

    Tiny linear simplification

--- pedestrians-video-2-carla/src/pedestrians_video_2_carla/pl_modules/linear.py
+++ pedestrians-video-2-carla/src/pedestrians_video_2_carla/pl_modules/linear.py
@@ -1,27 +1,27 @@
-from typing import Union
 import torch
 from torch import nn
 
 from pedestrians_video_2_carla.pl_modules.base import LitBaseMapper
-from pedestrians_video_2_carla.utils.openpose import BODY_25, COCO
-from pedestrians_video_2_carla.utils.unreal import CARLA_SKELETON
 
 
 class LitLinearMapper(LitBaseMapper):
-    def __init__(self, input_nodes: Union[BODY_25, COCO] = BODY_25, clip_length: int = 30):
-        super().__init__(input_nodes)
+    def __init__(self,
+                 clip_length: int = 30,
+                 **kwargs
+                 ):
+        super().__init__(**kwargs)
 
         self.__clip_length = clip_length
 
-        self.__input_nodes = len(input_nodes)
+        self.__input_nodes_len = len(self.projection.input_nodes)
         self.__input_features = 3  # OpenPose (x,y,confidence) points
 
-        self.__output_nodes = len(CARLA_SKELETON)
+        self.__output_nodes_len = len(self.projection.output_nodes)
         # bones rotations (euler angles; radians; roll, pitch, yaw) to get into the required position
         self.__output_features = 3
 
-        self.__input_size = self.__clip_length * self.__input_nodes * self.__input_features
-        self.__output_size = self.__clip_length * self.__output_nodes * self.__output_features
+        self.__input_size = self.__clip_length * self.__input_nodes_len * self.__input_features
+        self.__output_size = self.__clip_length * self.__output_nodes_len * self.__output_features
 
         self.pose_linear = nn.Linear(
             self.__input_size,
@@ -32,7 +32,7 @@ class LitLinearMapper(LitBaseMapper):
         x = x.reshape((-1, self.__input_size))
         pose_change = self.pose_linear(x)
         pose_change = pose_change.reshape(
-            (-1, self.__clip_length, self.__output_nodes, self.__output_features))
+            (-1, self.__clip_length, self.__output_nodes_len, self.__output_features))
         return pose_change
 
     def configure_optimizers(self):
