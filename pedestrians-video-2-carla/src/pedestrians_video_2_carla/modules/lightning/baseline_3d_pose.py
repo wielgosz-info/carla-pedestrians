4b6cff05b05716927409ad2e86e7b7228dcb8b86
Maciej Wielgosz <wielgosz@agh.edu.pl>
2021-11-07 13:40:25 +0100

    Add 3d_pose_baseline_pytorch submodule

---
+++ pedestrians-video-2-carla/src/pedestrians_video_2_carla/modules/lightning/baseline_3d_pose.py
@@ -0,0 +1,13 @@
+"""
+Based on the [PyTorch implementation](https://github.com/weigq/3d_pose_baseline_pytorch)
+of 3D pose baseline from the following paper:
+
+```bibtex
+@inproceedings{martinez_2017_3dbaseline,
+  title={A simple yet effective baseline for 3d human pose estimation},
+  author={Martinez, Julieta and Hossain, Rayat and Romero, Javier and Little, James J.},
+  booktitle={ICCV},
+  year={2017}
+}
+```
+"""
