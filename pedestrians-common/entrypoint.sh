955b0fe8d52e32514a17b2e851fff1277c755e20
Maciej Wielgosz <wielgosz@agh.edu.pl>
2022-01-30 11:58:45 +0100

    Remove pedestrins common, it will be better as submodule

--- pedestrians-common/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /app
-
-echo "Installing ${PACKAGE:-package}@${COMMIT}..."
-SETUPTOOLS_SCM_PRETEND_VERSION="0.0.post0.dev38+${COMMIT}.dirty" /venv/bin/python -m pip install --no-cache-dir -e /app
-
-echo "Executing CMD"
-exec "$@"
