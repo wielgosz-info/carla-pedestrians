c07da87d3ea7cec8114eddc643e70f2d3aefcee9
Maciej Wielgosz <wielgosz@agh.edu.pl>
2022-06-19 11:21:51 +0200

    Update OpenPose scripts to handle PIE dataset

--- openpose/extract_poses_from_dataset.sh
+++ openpose/extract_poses_from_dataset.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-dataset='JAAD'
+dataset='PIE'
 limit=360
 
 cd /openpose # it needs to be run there to find 'models' dir
 
 mkdir -p  /outputs/${dataset}
 i=1
-for filename in /datasets/${dataset}/videos/*.mp4; do
+for filename in /datasets/${dataset}/videos/**/*.mp4; do
+    set_name=$(dirname "$filename")
+    set_name=$(basename "$set_name")
+    mkdir -p  /outputs/${dataset}/${set_name}
+    
     name=$(basename "$filename" .mp4)
-    echo "Processing ${name}..."
+    echo "Processing ${set_name}/${name}..."
     ./build/examples/openpose/openpose.bin \
         --video ${filename} \
-        --write_json /outputs/${dataset}/${name} \
+        --write_json /outputs/${dataset}/${set_name}/${name} \
         --model_pose BODY_25 \
         --display 0 \
         --render_pose 0
